4977be61c7de6e3f5cd739cabf321573e867d8d8
Krzysztof Maj <s18533@pjwstk.edu.pl>
2020-10-14 17:07:12 +0200

    pushing broke code

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main(){
+	public static void main(String[] args){
+			printAreaOfSquare(5);
+	}
+	
+	public void printAreaOfSquare(int edge){
+		System.out.println("The area of the square equals " + (edge+edge));
+	}
+}
